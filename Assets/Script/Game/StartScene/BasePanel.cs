f7a9b54fa93cbdae6f0d5ed8c316009e0eaad057
Kaplc <755536175@qq.com>
2023-06-11 14:58:46 +0800

    完成场景基类和切换游戏场景

---
+++ Assets/Script/Game/StartScene/BasePanel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePanel<T> : MonoBehaviour where T : class
+{
+    private T _instance;
+    public T Instance => _instance;
+
+    private void Awake()
+    {
+        _instance = this as T;
+    }
+}
---
+++ Assets/Script/Game/StartScene/StartPanel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartPanel : BasePanel<StartPanel>
+{
+    public Button btnStart;
+    public Button btnSetting;
+    public Button btnEnd;
+    public Button btnRank;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnStart.ClickEvent += () =>
+        {
+            SceneManager.LoadScene("GameScene");
+        };
+        btnSetting.ClickEvent += () =>
+        {
+            
+        };
+        btnEnd.ClickEvent += () =>
+        {
+            Application.Quit(0);
+        };
+        btnRank.ClickEvent += () =>
+        {
+            
+        };
+        
+    }
+
+
+}
