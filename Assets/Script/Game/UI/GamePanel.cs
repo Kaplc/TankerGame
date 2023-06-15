014371fadb580331f1ad11ca5067982c320eae19
Kaplc <755536175@qq.com>
2023-06-15 13:41:57 +0800

    完成游戏面板

---
+++ Assets/Script/Game/UI/GamePanel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GamePanel : MonoBehaviour
+{
+    public Button btnClose;
+    public Button btnSetting;
+    public Texture txHp;
+    public Control.Label lbTime;
+    public Control.Label lbScore;
+    public int hpWidth;
+
+    [HideInInspector] public int score;
+
+    [HideInInspector] public float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnClose.ClickEvent += () => { };
+        btnSetting.ClickEvent += () => { };
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        lbTime.Content.text = (int)time + "秒";
+    }
+
+    public void AddScore(int add)
+    {
+        score += add;
+        lbScore.Content.text = score.ToString();
+    }
+
+    public void UpdateHp(int maxHp, int currHp)
+    {
+        txHp.GUIPos.Width = (float)currHp / maxHp * hpWidth;
+    }
+}
