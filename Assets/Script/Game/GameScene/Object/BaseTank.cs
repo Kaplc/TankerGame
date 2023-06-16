18d3807da2fe602cfde0ca5c246464fce70e05ba
Kaplc <755536175@qq.com>
2023-06-16 10:55:36 +0800

    完成坦克基类

---
+++ Assets/Script/Game/GameScene/Object/BaseTank.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseTank : MonoBehaviour
+{
+    public int atk;
+    public int def;
+    public int hp;
+
+    public int speed;
+    public float bodyRotation;
+    public float headRotation;
+
+    public GameObject deadEff;
+    
+    public abstract void  Move();
+    public abstract void  Shoot();
+
+    public virtual void Wound(BaseTank otherTank)
+    {
+        hp -= otherTank.atk - def;
+        
+        if (hp <= 0)
+        {
+            hp = 0;
+            Dead();
+        }
+    }
+    public virtual void Dead()
+    {
+        // 实例化死亡特效对象
+        GameObject deadEff = Instantiate(this.deadEff, transform.position, transform.rotation);
+        
+        // 控制特效音效
+        AudioSource audioSource = deadEff.GetComponent<AudioSource>();
+        audioSource.volume = BaseDataManage.Instance.musicData.musicValue;
+        audioSource.mute = !BaseDataManage.Instance.musicData.soundOpen;
+    }
+}
